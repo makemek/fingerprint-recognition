79037bacd3c915175b82a4f1b0a03e6a6d5e2f29
Makemek <makemek101@hotmail.com>
2015-11-11 12:35:25 +0700

    configure compile settings and tell how to run a program

--- Main.cpp
+++ Main.cpp
@@ -5,16 +5,31 @@
 #include "FpSegmentator.hpp"
 #include "FpMatcher.hpp"
 using namespace std;
-
 //----------------------------------------------------------------------
-int main() {
+
+/*
+To compile this program you need to 
+    1. Create environment variable call "opencv" points to (Your opencv installation path)\build\x86\vc12
+	2. Add %opencv%\bin to PATH environment variable
+	
+To run this program, you have to 
+    1. put 2 command line argument containing path to an image
+		1.1. path to input fingerprint image
+		1.2. path to fingerprint template image
+*/
+int main(int argc, char** argv) {
+	assert(argc == 2);
+
    //read fingerprint image 1
-   cv::Mat fpImg1 = cv::imread("FP_DB/1_1.bmp", CV_LOAD_IMAGE_GRAYSCALE);
-   cv::imshow("fp1", fpImg1);
+   cv::Mat fpImg1 = cv::imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
 
    //read fingerprint image 2
-   cv::Mat fpImg2 = cv::imread("FP_DB/1_1.bmp", CV_LOAD_IMAGE_GRAYSCALE);
-   cv::imshow("fp2", fpImg1);
+   cv::Mat fpImg2 = cv::imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
+   
+   assert(fpImg1.data && fpImg2.data);
+
+   cv::imshow("fingerprint1", fpImg1);
+   cv::imshow("fingerprint2", fpImg2);
 
    //match two fingerprint images
    FpMatcher fpMatcher;
---
+++ README.txt
@@ -0,0 +1,8 @@
+To compile this program you need to 
+    1. Create environment variable call "opencv" points to (Your opencv installation path)\build\x86\vc12
+	2. Add %opencv%\bin to PATH environment variable
+	
+To run this program, you have to 
+    1. put 2 command line argument containing path to an image
+		1.1. path to input fingerprint image
+		1.2. path to fingerprint template image
