030bbd4375d6a3f18870cbd54658cb0af5d34f73
Makemek <makemek101@hotmail.com>
2015-11-12 00:17:23 +0700

    implement feature hasFingerPrintLine

--- Components/Segmentor/FpSegmentator.cpp
+++ Components/Segmentor/FpSegmentator.cpp
@@ -1,15 +1,39 @@
 #include "FpSegmentator.hpp"
-
+#include <math.h>
 //----------------------------------------------------------------------
-void FpSegmentator::segment(const cv::Mat& fpImg, cv::Mat& maskImg) { 
+
+cv::Mat FpSegmentator::segment(const cv::Mat & fpImg)
+{
+	cv::Mat segmentedImg = fpImg.clone();
+	cv::bitwise_and(fpImg, getMask(fpImg), segmentedImg);
+	return segmentedImg;
+}
+
+bool FpSegmentator::hasFingerprintLine(const cv::Mat& fpImgBlock) {
    
-   cout << "Stub: fingerprint segmentation" << endl; 
+	assert(fpImgBlock.rows == fpImgBlock.cols);
 
-   maskImg = fpImg.clone();      //just copy
+	int totalPixelValue = 0, totalPixelSquareValue = 0;
+	for (auto it = fpImgBlock.begin<uchar>(); it != fpImgBlock.end<uchar>(); ++it) {
+		totalPixelValue += *it;
+		totalPixelSquareValue += pow(*it, 2);
+	}
+	
+	double average = 0, stdDeviation = 0;
+	int nPixel = fpImgBlock.rows * fpImgBlock.cols;
+	average = (double)totalPixelValue / nPixel;
+	stdDeviation = sqrt(totalPixelSquareValue / nPixel - pow(average, 2));
+	
+	cout << "avg " << average << ' ' << "std " << stdDeviation << endl;
+	return average < 40 && stdDeviation < 60;
+}
 
- 
-  
+cv::Mat FpSegmentator::getMask(const cv::Mat & fpImg)
+{
 
-
+	throw exception("Not implemented");
+	if (hasFingerprintLine(fpImg))
+		return cv::Mat::zeros(cv::Size(fpImg.rows, fpImg.cols), CV_8U);
+	
 }
 //----------------------------------------------------------------------
--- Components/Segmentor/FpSegmentator.hpp
+++ Components/Segmentor/FpSegmentator.hpp
@@ -19,16 +19,25 @@ public:
    //getters & setters
    
    //key methods
-   void segment(const cv::Mat& fpImg, cv::Mat& maskImg);
 
-   //operators
+	/*
+	Extract 
+	*/
+	cv::Mat segment(const cv::Mat& fpImg);
+
+	/*
+	Check if image has finger print presents
+	*/
+	bool hasFingerprintLine(const cv::Mat& fpImgBlock);
+
+	cv::Mat getMask(const cv::Mat& fpImg);
 
 private:
    //attributes
-	const static int FP_AREA = 0;
-	const static int NON_FP_AREA = 255;
+	const int FP_AREA = 0;
+	const int NON_FP_AREA = 255;
 
-	const static int BLOCK_SIZE = 16;
+	int block_size;
    
    //helper (private) methods
    
