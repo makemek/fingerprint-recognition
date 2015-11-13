0c8a57abd98f3c04346801d4bd959ea26c933fe0
Makemek <makemek101@hotmail.com>
2015-11-13 13:41:43 +0700

    Commit initial test project

---
+++ Components/Tests/EnhancerTest/OF_detectorTest.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "OfDetector.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace EnhancerTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestOrientalFieldDetector)
+		{
+
+		}
+
+	};
+}
