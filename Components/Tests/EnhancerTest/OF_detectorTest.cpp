7908403cdaae482a8400604d967224fa05ab2e56
Makemek <makemek101@hotmail.com>
2015-11-13 15:22:44 +0700

    Include test stub for oriental field

--- Components/Tests/EnhancerTest/OF_detectorTest.cpp
+++ Components/Tests/EnhancerTest/OF_detectorTest.cpp
@@ -12,7 +12,11 @@ namespace EnhancerTest
 		
 		TEST_METHOD(TestOrientalFieldDetector)
 		{
-
+			cv::Mat src = cv::imread("C:\\Users\\Apipol\\Desktop\\FP_DB\\1_2.BMP");
+			OfDetector detector = OfDetector();
+			cv::Mat field = detector.detect(cv::Size(16,16), src);
+			cv::imshow("out", field);
+			cv::waitKey();
 		}
 
 	};
