db75d4eb53060f72156881312e4fe0c543aa6b9e
Makemek <makemek101@hotmail.com>
2015-11-19 21:01:08 +0700

    add methods and attributes

--- Components/Enhancer/AngularField.cpp
+++ Components/Enhancer/AngularField.cpp
@@ -1,2 +1,26 @@
 #include "AngularField.hpp"
 
+AngularField::AngularField(const cv::Size & size, const cv::Mat & mat)
+{
+
+}
+
+AngularField::AngularField(const cv::Mat & mat)
+{
+
+}
+
+cv::Mat AngularField::visualize()
+{
+	return cv::Mat();
+}
+
+cv::Mat AngularField::getAngularMatrix()
+{
+	return cv::Mat();
+}
+
+cv::Size AngularField::getSize()
+{
+	return cv::Size();
+}
--- Components/Enhancer/AngularField.hpp
+++ Components/Enhancer/AngularField.hpp
@@ -1,6 +1,20 @@
 #ifndef ANGULAR_FIELD
 #define ANGULAR_FIELD
+#include "opencv2/core/core.hpp"
 
+class AngularField {
 
+public:
+	AngularField(const cv::Size& size, const cv::Mat& angleMat);
+	AngularField(const cv::Mat& mat);
+
+	cv::Mat visualize();
+	cv::Mat getAngularMatrix();
+	cv::Size getSize();
+
+private:
+	cv::Size size;
+	cv::Mat mat;
+};
 
 #endif
